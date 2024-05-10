42efe198d8948df20488979262fe65a70571bc69
jhoce <jhocelizf@gmail.com>
2024-05-10 13:00:42 -0300

    test

--- src/app/core/services/auth.services.ts
+++ src/app/core/services/auth.services.ts
@@ -6,45 +6,45 @@ import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private MOCK_AUTH_STUDENT: IStudent = {
-    id: 1,
-    createdAt: new Date(),
-    email: 'email@mail.com',
-    name: 'goku',
-    lastName: 'son',
-    role: 'ADMIN',
-};
+    private MOCK_AUTH_STUDENT: IStudent = {
+        id: 1,
+        createdAt: new Date(),
+        email: 'email@mail.com',
+        name: 'goku',
+        lastName: 'son',
+        role: 'ADMIN',
+    };
 
-private _authStudent$ = new BehaviorSubject<IStudent | null>(null);
-public authStudent$ = this._authStudent$.asObservable();
+    private _authStudent$ = new BehaviorSubject<IStudent | null>(null);
+    public authStudent$ = this._authStudent$.asObservable();
 
-constructor(private router: Router) {}
+    constructor(private router: Router) { }
 
-login(data: LoginData): void {
-    if (data.email !== 'user@mail.com' || data.password !== '123456') {
-      alert('Correo o password incorrectos');
-    } else {
-        this._authStudent$.next(this.MOCK_AUTH_STUDENT);
-        localStorage.setItem(
-        'accessToken',
-        'fdskfdsjkmngfunudsijfdsioufjsdoifdsyhfds'
-    );
-    this.router.navigate(['dashboard', 'home']);
+    login(data: LoginData): void {
+        if (data.email !== 'user@mail.com' || data.password !== '123456') {
+            alert('Correo o password incorrectos');
+        } else {
+            this._authStudent$.next(this.MOCK_AUTH_STUDENT);
+            localStorage.setItem(
+                'accessToken',
+                'fdskfdsjkmngfunudsijfdsioufjsdoifdsyhfds'
+            );
+            this.router.navigate(['dashboard', 'home']);
+        }
     }
-}
 
-verifyToken(): boolean {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-        this._authStudent$.next(this.MOCK_AUTH_STUDENT);
-        return true;
-    } else {
-        return false;
+    verifyToken(): boolean {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            this._authStudent$.next(this.MOCK_AUTH_STUDENT);
+            return true;
+        } else {
+            return false;
+        }
     }
-}
 
-logout(): void {
-    this._authStudent$.next(null);
-    localStorage.removeItem('accessToken');
-}
+    logout(): void {
+        this._authStudent$.next(null);
+        localStorage.removeItem('accessToken');
+    }
 }
